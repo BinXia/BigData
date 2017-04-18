fb7c03b9b2ac76e6062400e4cbb12298b7569ba2
Kryolith <ben.binxia@gmail.com>
2017-04-18 20:42:48 +0800

    更改数据集位置设置

--- Codes/DeepLearning/SoftmaxRegression/SoftReg(empty).py
+++ Codes/DeepLearning/SoftmaxRegression/SoftReg(empty).py
@@ -131,7 +131,7 @@ def main(_):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+  parser.add_argument('--data_dir', type=str, default='../Dataset',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
